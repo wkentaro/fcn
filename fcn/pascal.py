bf9ed685500e4afe59ae908af17ef9dc4d5a6dbf
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-05-04 01:40:47 +0900

    Add fcn.pascal

--- fcn/pascal.py
+++ fcn/pascal.py
@@ -1,3 +1,28 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import numpy as np
 
+
+def get_target_names():
+    target_names = [
+        'background',
+        'aeroplane',
+        'bicycle',
+        'bird',
+        'boat',
+        'bottle',
+        'bus',
+        'car',
+        'cat',
+        'chair',
+        'cow',
+        'diningtable',
+        'dog',
+        'horse',
+        'motorbike',
+        'person',
+        'potted plant',
+        'sheep',
+        'sofa',
+        'train',
+        'tv/monitor',
+    ]
+    return np.array(target_names)
