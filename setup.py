9a1e0420e8480b3d18511fd18e4a5cb49be9601a
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-05-05 01:47:52 +0900

    Download chainermodel before install

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
+from distutils.command.install_data import install_data
 import shlex
 import subprocess
 import sys
+import tempfile
 
 from setuptools import find_packages
 from setuptools import setup
@@ -22,11 +24,20 @@ if sys.argv[-1] == 'release':
     sys.exit(0)
 
 
+class FcnInstallData(install_data):
+    def run(self):
+        url = 'https://drive.google.com/uc?id=0B9P1L--7Wd2veTdBQWZybENLWmM'
+        output = tempfile.mktemp()
+        subprocess.check_call(['gdown', '-q', url, '-O', output])
+        self.data_files.append(output)
+        super(__class__, self).run()
+
+
 setup(
     name='fcn',
     version=version,
     packages=find_packages(),
-    package_data={'fcn': ['_data/fcn8s.chainermodel']},
+    cmdclass={'install_data': install_data},
     scripts=['scripts/fcn_forward.py'],
     install_requires=open('requirements.txt').readlines(),
     description='Fully Convolutional Networks',
