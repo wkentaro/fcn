da6e9416e12ce71cd3f23ded9bd75dccc62d26fe
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-05-04 22:31:16 +0900

    Move data directory in package

--- fcn/config.py
+++ fcn/config.py
@@ -3,9 +3,4 @@ import os.path as osp
 
 def get_data_dir():
     this_dir = osp.dirname(osp.abspath(__file__))
-    return osp.realpath(osp.join(this_dir, '../data'))
-
-
-def get_logs_dir():
-    this_dir = osp.dirname(osp.abspath(__file__))
-    return osp.realpath(osp.join(this_dir, '../logs'))
+    return osp.realpath(osp.join(this_dir, '_data'))
