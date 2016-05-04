4bb8222a88a8f77618f16c7b1dfec6079458a1f8
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-05-04 22:45:13 +0900

    Install forward.py

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     version=version,
     packages=find_packages(),
     package_data={'fcn': ['_data/fcn8s.chainermodel']},
+    scripts=['scripts/forward.py'],
     install_requires=open('requirements.txt').readlines(),
     author='Kentaro Wada',
     author_email='www.kentaro.wada@gmail.com',
