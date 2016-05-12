5749370480f7ef18c0ec30c608afe7c945dc527c
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-05-13 00:07:06 +0900

    Add model of FCN16s

--- fcn/models/__init__.py
+++ fcn/models/__init__.py
@@ -3,5 +3,6 @@ from __future__ import division
 from __future__ import print_function
 
 from fcn.models.fcn8s import FCN8s
+from fcn.models.fcn16s import FCN16s
 from fcn.models.fcn32s import FCN32s
 from fcn.models.vgg16 import VGG16
---
+++ fcn/models/fcn16s.py
@@ -0,0 +1,147 @@
+import chainer
+from chainer import cuda
+import chainer.functions as F
+import chainer.links as L
+from chainer import Variable
+import numpy as np
+
+
+class FCN16s(chainer.Chain):
+
+    """Full Convolutional Network 16s"""
+
+    def __init__(self, n_class=21):
+        self.n_class = n_class
+        super(self.__class__, self).__init__(
+            conv1_1=L.Convolution2D(3, 64, 3, stride=1, pad=100),
+            conv1_2=L.Convolution2D(64, 64, 3, stride=1, pad=1),
+
+            conv2_1=L.Convolution2D(64, 128, 3, stride=1, pad=1),
+            conv2_2=L.Convolution2D(128, 128, 3, stride=1, pad=1),
+
+            conv3_1=L.Convolution2D(128, 256, 3, stride=1, pad=1),
+            conv3_2=L.Convolution2D(256, 256, 3, stride=1, pad=1),
+            conv3_3=L.Convolution2D(256, 256, 3, stride=1, pad=1),
+
+            conv4_1=L.Convolution2D(256, 512, 3, stride=1, pad=1),
+            conv4_2=L.Convolution2D(512, 512, 3, stride=1, pad=1),
+            conv4_3=L.Convolution2D(512, 512, 3, stride=1, pad=1),
+
+            conv5_1=L.Convolution2D(512, 512, 3, stride=1, pad=1),
+            conv5_2=L.Convolution2D(512, 512, 3, stride=1, pad=1),
+            conv5_3=L.Convolution2D(512, 512, 3, stride=1, pad=1),
+
+            fc6=L.Convolution2D(512, 4096, 7, stride=1, pad=0),
+            fc7=L.Convolution2D(4096, 4096, 1, stride=1, pad=0),
+
+            score_fr=L.Convolution2D(4096, self.n_class, 1, stride=1, pad=0),
+            score_pool4=L.Convolution2D(512, self.n_class, 1, stride=1, pad=0),
+
+            upscore2=L.Deconvolution2D(self.n_class, self.n_class, 4,
+                                       stride=2),
+            upscore16=L.Deconvolution2D(self.n_class, self.n_class, 32,
+                                        stride=16),
+        )
+        self.train = False
+
+    def __call__(self, x, t=None):
+        # conv1
+        h = F.relu(self.conv1_1(x))
+        conv1_1 = h
+        h = F.relu(self.conv1_2(conv1_1))
+        conv1_2 = h
+        h = F.max_pooling_2d(conv1_2, 2, stride=2, pad=0)
+        pool1 = h  # 1/2
+
+        # conv2
+        h = F.relu(self.conv2_1(pool1))
+        conv2_1 = h
+        h = F.relu(self.conv2_2(conv2_1))
+        conv2_2 = h
+        h = F.max_pooling_2d(conv2_2, 2, stride=2, pad=0)
+        pool2 = h  # 1/4
+
+        # conv3
+        h = F.relu(self.conv3_1(pool2))
+        conv3_1 = h
+        h = F.relu(self.conv3_2(conv3_1))
+        conv3_2 = h
+        h = F.relu(self.conv3_3(conv3_2))
+        conv3_3 = h
+        h = F.max_pooling_2d(conv3_3, 2, stride=2, pad=0)
+        pool3 = h  # 1/8
+
+        # conv4
+        h = F.relu(self.conv4_1(pool3))
+        h = F.relu(self.conv4_2(h))
+        h = F.relu(self.conv4_3(h))
+        h = F.max_pooling_2d(h, 2, stride=2, pad=0)
+        pool4 = h  # 1/16
+
+        # conv5
+        h = F.relu(self.conv5_1(pool4))
+        h = F.relu(self.conv5_2(h))
+        h = F.relu(self.conv5_3(h))
+        h = F.max_pooling_2d(h, 2, stride=2, pad=0)
+        pool5 = h  # 1/32
+
+        # fc6
+        h = F.relu(self.fc6(pool5))
+        h = F.dropout(h, ratio=.5, train=self.train)
+        fc6 = h  # 1/32
+
+        # fc7
+        h = F.relu(self.fc7(fc6))
+        h = F.dropout(h, ratio=.5, train=self.train)
+        fc7 = h  # 1/32
+
+        # score_fr
+        h = self.score_fr(fc7)
+        score_fr = h  # 1/32
+
+        # upscore2
+        h = self.upscore(score_fr)
+        upscore2 = h  # 1/16
+
+        # score_pool4
+        h = self.score_pool4(pool4)
+        score_pool4 = h
+
+        # score_pool4c
+        h = F.crop(score_pool4, upscore2, axes=[2, 3], offset=5)
+        score_pool4c = h
+
+        # fuse_pool4
+        h = upscore2 + score_pool4c
+        fuse_pool4 = h
+
+        # upscore16
+        h = self.upscore16(fuse_pool4)
+        upscore16 = h
+
+        # score
+        h = F.crop(upscore16, x, axes=[2, 3], offset=27)
+        score = h  # 1/1
+
+        # testing without t
+        self.pred = score
+        if t is None:
+            return self.pred
+
+        # testing with t or training
+        self.accuracy = self.accuracy_score(self.pred, t)
+        self.loss = F.softmax_cross_entropy(self.pred, t, normalize=False)
+        if np.isnan(cuda.to_cpu(self.loss.data)).sum() != 0:
+            raise RuntimeError('ERROR in FCN8s: loss.data contains nan')
+        return self.loss
+
+    def accuracy_score(self, y_pred, y_true):
+        y_pred = cuda.to_cpu(y_pred.data)
+        y_true = cuda.to_cpu(y_true.data)
+        # reduce values along classes axis
+        reduced_y_pred = np.argmax(y_pred, axis=1)
+        assert reduced_y_pred.ndim == 3
+        assert reduced_y_pred.shape == y_true.shape
+        mask = y_true != -1
+        score = (reduced_y_pred[mask] == y_true[mask]).mean()
+        return Variable(np.array([score]), volatile=True)
