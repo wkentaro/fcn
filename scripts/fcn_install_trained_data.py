0ebbe21810f6fde7e3a516366f993e49b6ef1065
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-06-25 20:07:31 +0900

    Fix fcn_install_trained_data.py

--- scripts/fcn_install_trained_data.py
+++ scripts/fcn_install_trained_data.py
@@ -4,6 +4,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os.path as osp
+
+import fcn
 from fcn.util import download_data
 
 
