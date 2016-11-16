1a14619f1cab80cfe36709c24c52a9dfee55f5d8
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-11-17 05:39:15 +0900

    Update learning curve to visualize the trainer log

--- scripts/learning_curve.py
+++ scripts/learning_curve.py
@@ -11,10 +11,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def learning_curve(csv_file):
-    df = pd.read_csv(csv_file)
-    df_train  = df.query("type == 'train'")
-    df_val = df.query("type == 'val'")
+def learning_curve(json_file):
+    df = pd.read_json(json_file)
 
     colors = sns.husl_palette(3, l=.5, s=.5)
 
@@ -24,28 +22,38 @@ def learning_curve(csv_file):
     # TRAIN #
     #########
 
+    columns = [
+        'iteration',
+        'main/loss',
+        'main/accuracy',
+        'main/iu',
+    ]
+    df_train = df[columns]
+
     # train loss
     plt.subplot(231)
-    plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-    plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-    plt.plot(df_train.i_iter, df_train.loss, '-', markersize=1, color=colors[0],
-             alpha=.5, label='train loss')
+    plt.ticklabel_format(style='sci', axis='x', scilimits=(0, 0))
+    plt.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+    plt.plot(df_train['iteration'], df_train['main/loss'],
+             '-', markersize=1, color=colors[0], alpha=.5, label='train loss')
     plt.xlabel('iteration')
     plt.ylabel('train loss')
 
     # train accuracy
     plt.subplot(232)
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-    plt.plot(df_train.i_iter, df_train.acc, '-', markersize=1, color=colors[1],
-             alpha=.5, label='train accuracy')
+    plt.plot(df_train['iteration'], df_train['main/accuracy'],
+             '-', markersize=1, color=colors[1], alpha=.5,
+             label='train accuracy')
     plt.xlabel('iteration')
     plt.ylabel('train overall accuracy')
 
     # train mean iu
     plt.subplot(233)
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-    plt.plot(df_train.i_iter, df_train.iu, '-', markersize=1, color=colors[2],
-             alpha=.5, label='train accuracy')
+    plt.plot(df_train['iteration'], df_train['main/iu'],
+             '-', markersize=1, color=colors[2], alpha=.5,
+             label='train accuracy')
     plt.xlabel('iteration')
     plt.ylabel('train mean IU')
 
@@ -53,41 +61,50 @@ def learning_curve(csv_file):
     # VAL #
     #######
 
+    columns = [
+        'iteration',
+        'validation/main/loss',
+        'validation/main/accuracy',
+        'validation/main/iu',
+    ]
+    df_val = df[columns]
+    df_val = df_val.dropna()
+
     # val loss
     plt.subplot(234)
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
     plt.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-    plt.plot(df_val.i_iter, df_val.loss, 'o-', color=colors[0],
-             alpha=.5, label='val loss')
+    plt.plot(df_val['iteration'], df_val['validation/main/loss'],
+             'o-', color=colors[0], alpha=.5, label='val loss')
     plt.xlabel('iteration')
     plt.ylabel('val loss')
 
     # val accuracy
     plt.subplot(235)
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-    plt.plot(df_val.i_iter, df_val.acc, 'o-', color=colors[1],
-             alpha=.5, label='val accuracy')
+    plt.plot(df_val['iteration'], df_val['validation/main/accuracy'],
+             'o-', color=colors[1], alpha=.5, label='val accuracy')
     plt.xlabel('iteration')
     plt.ylabel('val overall accuracy')
 
     # val mean iu
     plt.subplot(236)
     plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-    plt.plot(df_val.i_iter, df_val.iu, 'o-', color=colors[2],
-             alpha=.5, label='val mean IU')
+    plt.plot(df_val['iteration'], df_val['validation/main/iu'],
+             'o-', color=colors[2], alpha=.5, label='val mean IU')
     plt.xlabel('iteration')
     plt.ylabel('val mean IU')
 
-    fig_file = osp.splitext(csv_file)[0] + '.png'
+    fig_file = osp.splitext(json_file)[0] + '.png'
     plt.savefig(fig_file)
     print('Saved to %s' % fig_file)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('csv_file')
+    parser.add_argument('json_file')
     args = parser.parse_args()
-    learning_curve(args.csv_file)
+    learning_curve(args.json_file)
 
 
 if __name__ == '__main__':
