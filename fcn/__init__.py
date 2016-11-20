50e929f2ca2971d7a084f4bc27a77901fb35afcb
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-11-20 23:27:38 +0900

    Add trainers module

--- fcn/__init__.py
+++ fcn/__init__.py
@@ -1,4 +1,6 @@
 import fcn.data  # NOQA
 import fcn.datasets  # NOQA
 import fcn.models  # NOQA
+import fcn.trainers  # NOQA
+import fcn.training  # NOQA
 import fcn.util  # NOQA
---
+++ fcn/training/__init__.py
@@ -0,0 +1,1 @@
+import fcn.training.extensions  # NOQA
