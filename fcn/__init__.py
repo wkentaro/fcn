8beb747087f9787cedfc3ea090a5c41b17cf6d7e
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-11-21 03:52:28 +0900

    Make inferencer as module

--- fcn/__init__.py
+++ fcn/__init__.py
@@ -1,6 +1,10 @@
 import fcn.data  # NOQA
 import fcn.datasets  # NOQA
+import fcn.inferencer  # NOQA
 import fcn.models  # NOQA
 import fcn.trainers  # NOQA
 import fcn.training  # NOQA
 import fcn.util  # NOQA
+
+
+Inferencer = fcn.inferencer.Inferencer
