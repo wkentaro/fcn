20328d1e2f417c7b38c9e797f50c3c536153061d
Kentaro Wada <www.kentaro.wada@gmail.com>
2016-11-24 04:04:29 +0900

    Rename fcn.util to fcn.utils

--- fcn/__init__.py
+++ fcn/__init__.py
@@ -4,7 +4,8 @@ import fcn.inferencer  # NOQA
 import fcn.models  # NOQA
 import fcn.trainers  # NOQA
 import fcn.training  # NOQA
-import fcn.util  # NOQA
+import fcn.utils  # NOQA
+import fcn.utils as util  # For Backward-compatibility  # NOQA
 
 
 Inferencer = fcn.inferencer.Inferencer
