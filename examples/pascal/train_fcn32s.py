4cdafd57bdf6ff4d840d6da9eb0c5f5c952086b8
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-04-06 09:02:53 +0900

    Improve trainer and pascal example

--- examples/pascal/train_fcn32s.py
+++ examples/pascal/train_fcn32s.py
@@ -1,53 +1,73 @@
 #!/usr/bin/env python
 
-import argparse
+import datetime
 import os
 import os.path as osp
+import sys
 
 import chainer
 from chainer import cuda
+import click
+import pytz
+import yaml
 
 import fcn
 from fcn.datasets import PascalVOC2012SegmentationDataset
 
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--gpu', type=int, default=0)
-    parser.add_argument('--out', required=True)
-    args = parser.parse_args()
+this_dir = osp.dirname(osp.abspath(__file__))
 
-    gpu = args.gpu
-    out = args.out
 
-    if not osp.exists(out):
-        os.makedirs(out)
+@click.command()
+@click.argument('config_file', type=click.Path(exists=True))
+@click.option('-g', '--gpu', type=int, required=True)
+def main(gpu, config_file):
+    # 0. config
+
+    config = yaml.load(open(config_file))
+
+    out = osp.splitext(osp.basename(config_file))[0]
+    for key, value in sorted(config.items()):
+        if key == 'name':
+            continue
+        if isinstance(value, basestring):
+            value = value.replace('/', 'SLASH')
+            value = value.replace(':', 'COLON')
+        out += '_{key}-{value}'.format(key=key.upper(), value=value)
+    config['out'] = osp.join(this_dir, 'logs', config['name'], out)
+
+    config['config_file'] = osp.realpath(config_file)
+    config['timestamp'] = datetime.datetime.now(
+        pytz.timezone('Asia/Tokyo')).isoformat()
+    if not osp.exists(config['out']):
+        os.makedirs(config['out'])
+    with open(osp.join(config['out'], 'config.yaml'), 'w') as f:
+        yaml.safe_dump(config, f, default_flow_style=False)
+    yaml.safe_dump(config, sys.stderr, default_flow_style=False)
 
     # 1. dataset
 
     dataset_train = PascalVOC2012SegmentationDataset('train')
     dataset_val = PascalVOC2012SegmentationDataset('val')
 
-    iter_train = chainer.iterators.SerialIterator(dataset_train, batch_size=1)
-    iter_val = chainer.iterators.SerialIterator(dataset_val, batch_size=1,
-                                                repeat=False, shuffle=False)
+    iter_train = chainer.iterators.MultiprocessIterator(
+        dataset_train, batch_size=1, shared_mem=10**7)
+    iter_valid = chainer.iterators.MultiprocessIterator(
+        dataset_val, batch_size=1, shared_mem=10**7,
+        repeat=False, shuffle=False)
 
     # 2. model
 
     n_class = len(dataset_train.label_names)
 
-    vgg_path = fcn.data.download_vgg16_chainermodel()
+    vgg_path = osp.join(chainer.dataset.get_dataset_directory('fcn'),
+                        'vgg16.chainermodel')
     vgg = fcn.models.VGG16()
     chainer.serializers.load_hdf5(vgg_path, vgg)
 
     model = fcn.models.FCN32s(n_class=n_class)
+    model.init_from_vgg16(vgg, copy_fc8=True, init_upscore=False)
     model.train = True
-    fcn.utils.copy_chainermodel(vgg, model)
-    for link_name in ['fc6', 'fc7']:
-        W1, b1 = getattr(vgg, link_name).params()
-        W2, b2 = getattr(model, link_name).params()
-        W2.data = W1.data.reshape(W2.shape)
-        b2.data = b1.data
 
     if gpu >= 0:
         cuda.get_device(gpu).use()
@@ -55,8 +75,9 @@ def main():
 
     # 3. optimizer
 
-    optimizer = chainer.optimizers.Adam(alpha=1e-5)
+    optimizer = chainer.optimizers.Adam(alpha=config['lr'])
     optimizer.setup(model)
+    optimizer.add_hook(chainer.optimizer.WeightDecay(rate=0.0005))
 
     # training loop
 
@@ -65,13 +86,10 @@ def main():
         model=model,
         optimizer=optimizer,
         iter_train=iter_train,
-        iter_val=iter_val,
-        out=out,
+        iter_valid=iter_valid,
+        out=config['out'],
     )
-    trainer.train(
-        max_iter=150000,
-        interval_eval=5000,
-    )
+    trainer.train(max_iter=config['max_iteration'])
 
 
 if __name__ == '__main__':
--- fcn/trainer.py
+++ fcn/trainer.py
@@ -1,3 +1,4 @@
+import collections
 import copy
 import os
 import os.path as osp
@@ -20,33 +21,50 @@ class Trainer(object):
             model,
             optimizer,
             iter_train,
-            iter_val,
+            iter_valid,
             out,
             ):
         self.device = device
         self.model = model
         self.optimizer = optimizer
         self.iter_train = iter_train
-        self.iter_val = iter_val
+        self.iter_valid = iter_valid
         self.out = out
         self.epoch = 0
         self.iteration = 0
+        self.log_headers = [
+            'epoch',
+            'iteration',
+            'train/loss',
+            'train/acc',
+            'train/acc_cls',
+            'train/mean_iu',
+            'train/fwavacc',
+            'valid/loss',
+            'valid/acc',
+            'valid/acc_cls',
+            'valid/mean_iu',
+            'valid/fwavacc',
+        ]
+        if not osp.exists(osp.join(self.out, 'log.csv')):
+            with open(osp.join(self.out, 'log.csv'), 'w') as f:
+                f.write(','.join(self.log_headers) + '\n')
 
     def evaluate(self, n_viz=9):
-        iter_val = copy.copy(self.iter_val)
+        iter_valid = copy.copy(self.iter_valid)
         self.model.train = False
         logs = []
         vizs = []
-        dataset = iter_val.dataset
+        dataset = iter_valid.dataset
         interval = len(dataset) // n_viz
-        desc = 'eval [iter=%d]' % self.iteration
-        for batch in tqdm.tqdm(iter_val, desc=desc, total=len(dataset),
+        desc = 'eval [epoch=%d]' % self.epoch
+        for batch in tqdm.tqdm(iter_valid, desc=desc, total=len(dataset),
                                ncols=80, leave=False):
             in_vars = utils.batch_to_vars(
                 batch, device=self.device, volatile=True)
             self.model(*in_vars)
             logs.append(self.model.log)
-            if iter_val.current_position % interval == 0 and \
+            if iter_valid.current_position % interval == 0 and \
                     len(vizs) < n_viz:
                 img = dataset.datum_to_img(self.model.data[0])
                 viz = utils.visualize_segmentation(
@@ -54,19 +72,19 @@ class Trainer(object):
                     n_class=self.model.n_class)
                 vizs.append(viz)
         # save visualization
-        out_viz = osp.join(self.out, 'viz_eval', 'iter%d.jpg' % self.iteration)
+        out_viz = osp.join(self.out, 'viz_eval', 'epoch%d.jpg' % self.epoch)
         if not osp.exists(osp.dirname(out_viz)):
             os.makedirs(osp.dirname(out_viz))
         viz = fcn.utils.get_tile_image(vizs)
         skimage.io.imsave(out_viz, viz)
         # generate log
         log = pd.DataFrame(logs).mean(axis=0).to_dict()
-        log = {'validation/%s' % k: v for k, v in log.items()}
+        log = {'valid/%s' % k: v for k, v in log.items()}
         # finalize
         self.model.train = True
         return log
 
-    def train(self, max_iter, interval_eval):
+    def train(self, max_iter):
         for iteration, batch in tqdm.tqdm(enumerate(self.iter_train),
                                           desc='train', total=max_iter,
                                           ncols=80):
@@ -77,15 +95,21 @@ class Trainer(object):
             # evaluate #
             ############
 
-            log_val = {}
-            if iteration % interval_eval == 0:
-                log_val = self.evaluate()
+            if self.iteration == 0 or self.iter_train.is_new_epoch:
+                log = collections.defaultdict(str)
+                log_valid = self.evaluate()
+                log.update(log_valid)
+                log['epoch'] = self.iter_train.epoch
+                log['iteration'] = iteration
+                with open(osp.join(self.out, 'log.csv'), 'a') as f:
+                    f.write(','.join(str(log[h]) for h in self.log_headers) +
+                            '\n')
                 out_model_dir = osp.join(self.out, 'models')
                 if not osp.exists(out_model_dir):
                     os.makedirs(out_model_dir)
                 out_model = osp.join(
-                    out_model_dir, '%s_iter%d.h5' %
-                    (self.model.__class__.__name__, self.iteration))
+                    out_model_dir, '%s_epoch%d.h5' %
+                    (self.model.__class__.__name__, self.epoch))
                 chainer.serializers.save_hdf5(out_model, self.model)
 
             #########
@@ -100,12 +124,15 @@ class Trainer(object):
             if loss is not None:
                 loss.backward()
                 self.optimizer.update()
-                log = self.model.log
+                log = collections.defaultdict(str)
+                log_train = {'train/%s' % k: v
+                             for k, v in self.model.log.items()}
                 log['epoch'] = self.iter_train.epoch
                 log['iteration'] = iteration
-                log.update(log_val)
-                utils.append_log_to_json(self.model.log,
-                                         osp.join(self.out, 'log.json'))
+                log.update(log_train)
+                with open(osp.join(self.out, 'log.csv'), 'a') as f:
+                    f.write(','.join(str(log[h]) for h in self.log_headers) +
+                            '\n')
 
             if iteration >= max_iter:
                 break
