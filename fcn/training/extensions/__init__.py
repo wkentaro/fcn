ba0393c663545276cae469e62eb41dda2db4f659
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-06-06 01:06:27 +0900

    Remove no need training module

--- fcn/training/extensions/__init__.py
+++
@@ -1,8 +0,0 @@
-from fcn.training.extensions import test_mode_evaluator
-
-from fcn.training.extensions import image_visualizer
-
-
-TestModeEvaluator = test_mode_evaluator.TestModeEvaluator
-
-ImageVisualizer = image_visualizer.ImageVisualizer
