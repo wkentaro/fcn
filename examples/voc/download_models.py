e5e023cdbaf6996a6f26726ee63f551d5b73b313
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-06-06 02:39:24 +0900

    Add script to download pre-trained models

---
+++ examples/voc/download_models.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import os.path as osp
+
+import fcn
+
+
+def main():
+    # models converted from caffe
+    path = fcn.data.download_vgg16_chainermodel()
+    print('==> downloaded to: %s' % path)
+    path = fcn.data.download_fcn32s_chainermodel()
+    print('==> downloaded to: %s' % path)
+    path = fcn.data.download_fcn16s_chainermodel()
+    print('==> downloaded to: %s' % path)
+    path = fcn.data.download_fcn8s_chainermodel()
+    print('==> downloaded to: %s' % path)
+
+
+if __name__ == '__main__':
+    main()
